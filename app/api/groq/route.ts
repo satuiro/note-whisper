f2f9d1f5ed6b7fc6175ec91c5df908c39ec11613
satuiro <mohakmalhotra008@gmail.com>
2024-04-03 22:28:03 +0530

    Fixed build error

--- app/api/groq/route.ts
+++ app/api/groq/route.ts
@@ -4,7 +4,7 @@ import Groq from "groq-sdk";
 const groq = new Groq({
     apiKey: process.env.GROQ_API_KEY
 });
-async function chatResponse(transcribeText) {
+async function chatResponse(transcribeText: string) {
     const chatCompletion = await groq.chat.completions.create({
         "messages": [
             {
