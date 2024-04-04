361e627ea3c94fdb11be13fa6b46dde7fd6e3d9d
satuiro <mohakmalhotra008@gmail.com>
2024-04-04 23:08:28 +0530

    minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,32 @@
-import Image from "next/image";
-// import Microphone from "./microphone";
-import {SignInButton, SignUpButton} from "@clerk/nextjs";
-import {Button} from "@/components/ui/button";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/nextjs";
 import Link from "next/link";
-export default async function Home() {
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const user = useUser();
+  const router = useRouter();
+  console.log(user);
+
   return (
-   <main>
-       Home Page
-       <Button>
-           <Link href="/sign-up">
-               Sign-up
-           </Link>
-       </Button>
-       <Button>
-           <Link href="/sign-in">
-               Sign-in
-           </Link>
-       </Button>
-   </main>
+    <main>
+      <div className="flex flex-row justify-between mb:text-4xl">
+        <span className="ml-5">Home Page</span>
+        <div className="mt-1">
+          <Button className="mr-2">
+            <Link href="/sign-up">Sign-up</Link>
+          </Button>
+          {user.isSignedIn ? (
+            <Button className="mr-2" onClick={() => router.push("/dashboard")}>Sign-in</Button>
+          ) : (
+            <Button className="mr-2">
+              <Link href="/sign-in">Sign-in</Link>
+            </Button>
+          )}
+        </div>
+      </div>
+    </main>
   );
 }
